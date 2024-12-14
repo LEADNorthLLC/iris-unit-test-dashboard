c2e9f8de2a6288a4d75bdbed72430d8a79e81811
Shawntelle Coker <6152705+sncoker@users.noreply.github.com>
2024-12-14 09:13:09 -0600

    Fixed error in getShortLocation.ts

--- testing-dashboard-frontend/src/utils/getShortLocation.ts
+++ testing-dashboard-frontend/src/utils/getShortLocation.ts
@@ -1,4 +1,4 @@
-export function getShortClassName(fullName: string): string {
-    const parts = fullName.split('.');
-    return parts[parts.length - 1] || fullName;
-  }
+export function getShortLocation(location: string): string {
+  const parts = location.split('.');
+  return parts.length >= 2 ? `${parts[parts.length - 2]}.${parts[parts.length - 1]}` : location;
+}
